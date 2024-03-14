1b6ed8057d18b4efcaefe93e0e47e2863772674b
karynnemoreira <karynnemoreira@yahoo.com.br>
2024-03-14 18:22:39 -0300

    footer

--- src/componentes/Footer/Footer.jsx
+++ src/componentes/Footer/Footer.jsx
@@ -1,10 +1,10 @@
-import React, {useState} from "react"
+import React, { useState } from "react"
 
 function Footer() {
 
     //SETTIMEOUT: É UM MÉTODO QUE EXECUTARÁ UM BLOCO DE CÓDIGO APÓS UM TEMPO ESPECIFICADO (DELAY), O TEMPO É CORRESPONDIDO POR MILISSEGUNDOS, EXEMPLO: 3 SEGUNDOS EQUIVALE A 3000, 6 SEGGUNDOS EQUIVALE A 6000. 
 
-  // Sintaxe setTimeout
+    // Sintaxe setTimeout
     // setTimeout(() => {
     // o que eu quero que faça
     // }, tempo)
@@ -18,50 +18,48 @@ function Footer() {
         }, 5000)
     }
 
-//Exemplo 2 criando um contador usando o setTimeout
-  
-const [numero, setNumero] = useState(0)
+    //Exemplo 2 criando um contador usando o setTimeout
 
-const Add = () => {
-   setTimeout(()=>{
-    setNumero(numero + 1)
-   }, 3000)
-}
+    const [numero, setNumero] = useState(0)
 
-const Remover = () => {
-setTimeout(()=>{
-    setNumero(numero - 1)
-}, 2000)    
-}
+    const Add = () => {
+        setTimeout(() => {
+            setNumero(numero + 1)
+        }, 3000)
+    }
 
-
-//SETINTERVAL É UM MÉTODO QUE PERMITE EXECUTAR REPETIDADEMENTE UM BLOCO DE CÓDIGOS, A CADA INTERVALO DE TEMPO. EXEMPLO: DE 3 EM 3 SEGUNDOS FAÇA TAL COISA. 
-
-//SINTAXE SETINTERVAL
-// setInterval(()=>{
-//     o que quero fazer
-// }, tempo)
-
-//     estado  setEstado
-const [number, setNumber] = useState(0)
+    const Remover = () => {
+        setTimeout(() => {
+            setNumero(numero - 1)
+        }, 2000)
+    }
 
 
-function Start(){
-    setInterval(()=>{
-        setNumber((number) => number + 1) //retorne o seu valor + 1 
-    },1000)//retorne 1 valor de 1 em 1 segundo
-}
+    //SETINTERVAL É UM MÉTODO QUE PERMITE EXECUTAR REPETIDADEMENTE UM BLOCO DE CÓDIGOS, A CADA INTERVALO DE TEMPO. EXEMPLO: DE 3 EM 3 SEGUNDOS FAÇA TAL COISA. 
+
+    //SINTAXE SETINTERVAL
+    // setInterval(()=>{
+    //     o que quero fazer
+    // }, tempo)
+
+    //     estado  setEstado
+    const [number, setNumber] = useState(0)
 
 
-//-------------Criando a função start utilizando uma arrow function--------------
-// const Start =()=>{
-//     setInterval(()=>{
-//         setNumber((number) => number + 1) //retorne o seu valor + 1 
-//     },1000)//retorne 1 valor de 1 em 1 segundo
-// }
-//---------------------------------------------------------------------------------
+    function Start() {
+        setInterval(() => {
+            setNumber((number) => number + 1) //retorne o seu valor + 1 
+        }, 1000)//retorne 1 valor de 1 em 1 segundo
+    }
 
 
+    //-------------Criando a função start utilizando uma arrow function--------------
+    // const Start =()=>{
+    //     setInterval(()=>{
+    //         setNumber((number) => number + 1) //retorne o seu valor + 1 
+    //     },1000)//retorne 1 valor de 1 em 1 segundo
+    // }
+    //---------------------------------------------------------------------------------
 
     return (
         <footer>
@@ -74,7 +72,6 @@ function Start(){
                 <button onClick={Add}> Add + 1</button>
                 <button onClick={Remover}> Remover - 1</button>
             </div>
-
 
             <div className="boxCronometo">
                 <h2>Cronômetro</h2>
